4a492f47a04fb476cf7fbba78937c5c6943ea41a
mohsenqaysi <mohsen.qaysi@ucdconnect.ie>
2018-01-13 16:29:51 +0300

    Init commit

--- Feeding Circle Refresh/ViewController.swift
+++ Feeding Circle Refresh/ViewController.swift
@@ -9,17 +9,102 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var pulsingLayer:  CAShapeLayer!
+    
+    let containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .clear
+        return view
+    }()
+    
+    let circleViewOne: UIView = {
+        let view = UIView(frame: CGRect(x: 20, y: 0, width: 30, height: 30))
+        view.layer.cornerRadius = view.frame.height / 2
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    let circleViewTwo: UIView = {
+        let view = UIView(frame: CGRect(x: 60, y: 0, width: 30, height: 30))
+        view.layer.cornerRadius = view.frame.height / 2
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    let circleViewThree: UIView = {
+        let view = UIView(frame: CGRect(x: 60, y: 0, width: 30, height: 30))
+        view.layer.cornerRadius = view.frame.height / 2
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        return view
+    }()
+    
+    fileprivate func addViews() {
+        // View animations
+        view.addSubview(containerView)
+        containerView.addSubview(circleViewOne)
+        containerView.addSubview(circleViewTwo)
+        containerView.addSubview(circleViewThree)
+        
+        // add containerView to the center of the mani view
+        containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        containerView.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        containerView.heightAnchor.constraint(equalToConstant: 80).isActive = true
+        
+        // Add constaints to the circles
+        _ = circleViewOne.anchor(top: containerView.topAnchor, left: containerView.leftAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 80, bottomConstant: 0, rightConstant: 0, widthConstant: 30, heightConstant: 30)
+        
+        _ = circleViewTwo.anchor(top: containerView.topAnchor, left: circleViewOne.rightAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 20, bottomConstant: 0, rightConstant: 0, widthConstant: 30, heightConstant: 30)
+        
+        _ = circleViewThree.anchor(top: containerView.topAnchor, left: circleViewTwo.rightAnchor, bottom: nil, right: nil, topConstant: 12, leftConstant: 20, bottomConstant: 0, rightConstant: 0, widthConstant: 30, heightConstant: 30)
+        // animate the circles
+        circleViewOne.transform = CGAffineTransform(scaleX: 0, y: 0)
+        circleViewTwo.transform = CGAffineTransform(scaleX: 0, y: 0)
+        circleViewThree.transform = CGAffineTransform(scaleX: 0, y: 0)
+        
+        UIView.animate(withDuration: 0.7) {
+            self.circleViewOne.transform = .identity
+            self.circleViewTwo.transform = .identity
+            self.circleViewThree.transform = .identity
+        }
+        
+        UIView().startCustomAnimation(animatedView: circleViewOne, delay: 0.3, scaledByX: 0.5, scaledByY: 0.5)
+        UIView().startCustomAnimation(animatedView: circleViewTwo, delay: 0.6, scaledByX: 0.5, scaledByY: 0.5)
+        UIView().startCustomAnimation(animatedView: circleViewThree,delay: 0.9, scaledByX: 0.5, scaledByY: 0.5)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = UIColor(red:0.16, green:0.16, blue:0.16, alpha:1.0)
+        addViews()
     }
+}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+extension CAShapeLayer {
+    
+    func circleLayer(center: UIView, radius: CGFloat, fillColor: CGColor) -> CAShapeLayer {
+        // drow a circle
+        let shapeLayer = CAShapeLayer()
+        let ciclePath = UIBezierPath(arcCenter: center.center, radius: radius, startAngle: 0, endAngle: CGFloat.pi * 2 , clockwise: true)
+        shapeLayer.path = ciclePath.cgPath
+        shapeLayer.fillColor = fillColor
+        return shapeLayer
     }
+}
 
+extension UIView {
+    func startCustomAnimation(animatedView: UIView, delay: CGFloat = 0.5, scaledByX: CGFloat, scaledByY: CGFloat) {
 
+        UIView.animate(withDuration: 0.7, delay: TimeInterval(delay), usingSpringWithDamping: 1, initialSpringVelocity: 0, options: [.repeat,.autoreverse], animations: {
+            animatedView.transform = animatedView.transform.scaledBy(x: scaledByX, y: scaledByY)
+        }) { (completed) in
+            animatedView.transform = .identity
+        }
+    }
 }
 
---
+++ Feeding Circle Refresh/extensions.swift
@@ -0,0 +1,110 @@
+//
+//  extensions.swift
+//  UCD Menus
+//
+//  Created by Mohsen Qaysi on 12/3/17.
+//  Copyright © 2017 Mohsen Qaysi. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+
+extension UIView {
+    func viewCardTheme() {
+        // MARK: - CardView Layout
+        self.layer.cornerRadius = 3.0
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.black.withAlphaComponent(0.2).cgColor
+        self.layer.shadowOffset = CGSize(width: 0, height: 0)
+        self.layer.shadowOpacity = 0.8
+    }
+}
+
+extension CGFloat {
+    func getScreenWidth() -> CGFloat {
+        return UIScreen.main.bounds.width
+    }
+    
+    func getScreenHeight() -> CGFloat {
+        return UIScreen.main.bounds.height
+    }
+}
+
+extension UIView {
+    
+    func anchorToTop(_ top: NSLayoutYAxisAnchor? = nil,
+                     left: NSLayoutXAxisAnchor? = nil,
+                     bottom: NSLayoutYAxisAnchor? = nil,
+                     right: NSLayoutXAxisAnchor? = nil) {
+        
+        anchorWithConstantsToTop(
+            top,
+            left: left,
+            bottom: bottom,
+            right: right,
+            
+            topConstant: 0,
+            leftConstant: 0,
+            bottomConstant: 0,
+            rightConstant: 0)
+    }
+    
+    func anchorWithConstantsToTop(_ top: NSLayoutYAxisAnchor? = nil,
+                                  left: NSLayoutXAxisAnchor? = nil,
+                                  bottom: NSLayoutYAxisAnchor? = nil,
+                                  right: NSLayoutXAxisAnchor? = nil,
+                                  topConstant: CGFloat = 0,
+                                  leftConstant: CGFloat = 0,
+                                  bottomConstant: CGFloat = 0,
+                                  rightConstant: CGFloat = 0) {
+        
+        _ = anchor(top: top, left: left, bottom: bottom, right: right, topConstant: topConstant, leftConstant: leftConstant, bottomConstant: bottomConstant, rightConstant: rightConstant)
+    }
+    
+    func anchor(
+        top: NSLayoutYAxisAnchor? = nil,
+        left: NSLayoutXAxisAnchor? = nil,
+        bottom: NSLayoutYAxisAnchor? = nil,
+        right: NSLayoutXAxisAnchor? = nil,
+        topConstant: CGFloat = 0,
+        leftConstant: CGFloat = 0,
+        bottomConstant: CGFloat = 0,
+        rightConstant: CGFloat = 0,
+        widthConstant: CGFloat = 0,
+        heightConstant: CGFloat = 0) -> [NSLayoutConstraint] {
+        // Very importnat
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        var anchors = [NSLayoutConstraint]()
+        
+        if let top = top {
+            anchors.append(topAnchor.constraint(equalTo: top, constant: topConstant))
+        }
+        
+        if let left = left {
+            anchors.append(leftAnchor.constraint(equalTo: left, constant: leftConstant))
+        }
+        
+        if let bottom = bottom {
+            anchors.append(bottomAnchor.constraint(equalTo: bottom, constant: -bottomConstant))
+        }
+        
+        if let right = right {
+            anchors.append(rightAnchor.constraint(equalTo: right, constant: -rightConstant))
+        }
+        
+        if widthConstant > 0 {
+            anchors.append(widthAnchor.constraint(equalToConstant: widthConstant))
+        }
+        
+        if heightConstant > 0 {
+            anchors.append(heightAnchor.constraint(equalToConstant: heightConstant))
+        }
+        // Apply it to all of them "forEach"
+        anchors.forEach({$0.isActive = true})
+        
+        return anchors
+    }
+    
+}
